b4f2af11b456777609c01eab6faf27f36208418f
Shedou <chimbal.chimbal@yandex.ru>
2024-11-06 20:18:41 +0400

    new functions
    - Добавлена обработка более одного файла за раз

--- portsoft/script/chimbalix-scripts/context-menu/sendto/cl-desktop-launcher.sh
+++ portsoft/script/chimbalix-scripts/context-menu/sendto/cl-desktop-launcher.sh
@@ -6,46 +6,67 @@
 # TEMPLATE_MODE_TERMINAL
 # TEMPLATE_NAME
 # TEMPLATE_ICON
+# TEMPLATE_WORK_DIR
+# 
+# /usr/share/Thunar/sendto/cl-desktop-launcher.desktop
 
 source "$HOME/.config/user-dirs.dirs"
 
+All_ok=true
 Path_To_Script="$( dirname "$(readlink -f "$0")")"
-Path_To_EXE="$1"
-Mode_Terminal="false"
 Session_Number="$RANDOM"
 
-Template_Desktop_File="/portsoft/script/chimbalix-scripts/context-menu/sendto/cl-desktop-launcher-template.desktop"
-Input_EXE_Folder="$(dirname "$Path_To_EXE")"
-Input_EXE_File="$(basename "$Path_To_EXE")"
-Input_EXE_File_Without_Ext="${Input_EXE_File%.*}"
+Files=("$@")
 
-Output_Desktop_Dir="$XDG_DESKTOP_DIR"
-Output_Desktop_File="$Output_Desktop_Dir/$Input_EXE_File_Without_Ext.desktop"
-Output_Icon="/portsoft/script/chimbalix-scripts/context-menu/sendto/cl-desktop-launcher-template-icon.png"
-
-if [ -e "$Output_Desktop_File" ]; then
-	Output_Desktop_File="$Output_Desktop_Dir/$Input_EXE_File_Without_Ext""_$Session_Number"".desktop"; fi
-
-Icon_Check_EXE_PNG="$Input_EXE_Folder/$Input_EXE_File_Without_Ext.png"
-Icon_Check_EXE_SVG="$Input_EXE_Folder/$Input_EXE_File_Without_Ext.svg"
-Icon_Check_File_PNG="$Input_EXE_Folder/icon.png"
-Icon_Check_File_SVG="$Input_EXE_Folder/icon.svg"
-
-if [ -e "$Icon_Check_EXE_PNG" ]; then Output_Icon="$Icon_Check_EXE_PNG"; else
-	if [ -e "$Icon_Check_EXE_SVG" ]; then Output_Icon="$Icon_Check_EXE_SVG"; else
-		if [ -e "$Icon_Check_File_PNG" ]; then Output_Icon="$Icon_Check_File_PNG"; else
-			if [ -e "$Icon_Check_File_SVG" ]; then Output_Icon="$Icon_Check_File_SVG"; fi
+function LAUNCHER_CREATE() {
+	Path_To_EXE="$1"
+	Mode_Terminal="false"
+	
+	Template_Desktop_File="/portsoft/script/chimbalix-scripts/context-menu/sendto/cl-desktop-launcher-template.desktop"
+	Input_EXE_Folder="$(dirname "$Path_To_EXE")"
+	Input_EXE_File="$(basename "$Path_To_EXE")"
+	Input_EXE_File_Without_Ext="${Input_EXE_File%.*}"
+	
+	Output_Desktop_Dir="$XDG_DESKTOP_DIR"
+	Output_Desktop_File="$Output_Desktop_Dir/$Input_EXE_File_Without_Ext.desktop"
+	Output_Icon="/portsoft/script/chimbalix-scripts/context-menu/sendto/cl-desktop-launcher-template-icon.png"
+	
+	Icon_Check_EXE_PNG="$Input_EXE_Folder/$Input_EXE_File_Without_Ext.png"
+	Icon_Check_EXE_SVG="$Input_EXE_Folder/$Input_EXE_File_Without_Ext.svg"
+	Icon_Check_File_PNG="$Input_EXE_Folder/icon.png"
+	Icon_Check_File_SVG="$Input_EXE_Folder/icon.svg"
+	
+	if [ -e "$Output_Desktop_File" ]; then
+		Output_Desktop_File="$Output_Desktop_Dir/$Input_EXE_File_Without_Ext""_$Session_Number"".desktop"; fi
+	
+	if [ -e "$Icon_Check_EXE_PNG" ]; then Output_Icon="$Icon_Check_EXE_PNG"; else
+		if [ -e "$Icon_Check_EXE_SVG" ]; then Output_Icon="$Icon_Check_EXE_SVG"; else
+			if [ -e "$Icon_Check_File_PNG" ]; then Output_Icon="$Icon_Check_File_PNG"; else
+				if [ -e "$Icon_Check_File_SVG" ]; then Output_Icon="$Icon_Check_File_SVG"; fi
+			fi
 		fi
 	fi
-fi
+	
+	### If there are spaces in the path, running via terminal does not work regardless of escaping...
+	# MIME_Type="$(xdg-mime query filetype "$Path_To_EXE")"
+	# if [ "$MIME_Type" == "application/x-shellscript" ]; then Mode_Terminal="true"; fi
+	
+	if [ $All_ok == true ]; then
+		if cp "$Template_Desktop_File" "$Output_Desktop_File"; then
+			sed -i -e "s~TEMPLATE_PATH_TO_EXE~$Path_To_EXE~g" "$Output_Desktop_File"
+			sed -i -e "s~TEMPLATE_MODE_TERMINAL~$Mode_Terminal~g" "$Output_Desktop_File"
+			sed -i -e "s~TEMPLATE_NAME~$Input_EXE_File (Launcher)~g" "$Output_Desktop_File"
+			sed -i -e "s~TEMPLATE_ICON~$Output_Icon~g" "$Output_Desktop_File"
+			sed -i -e "s~TEMPLATE_WORK_DIR~$Input_EXE_Folder~g" "$Output_Desktop_File"
+		fi
+	fi
+}
 
-cp "$Template_Desktop_File" "$Output_Desktop_File"
-
-sed -i -e "s~TEMPLATE_PATH_TO_EXE~$Path_To_EXE~g" "$Output_Desktop_File"
-sed -i -e "s~TEMPLATE_MODE_TERMINAL~$Mode_Terminal~g" "$Output_Desktop_File"
-sed -i -e "s~TEMPLATE_NAME~$Input_EXE_File (Launcher)~g" "$Output_Desktop_File"
-sed -i -e "s~TEMPLATE_ICON~$Output_Icon~g" "$Output_Desktop_File"
-
+for i in "${!Files[@]}"; do
+	CurrentFile="${Files[$i]}"
+	Current_Array_Number="$i"
+	LAUNCHER_CREATE "$CurrentFile"
+done
 
 # MIT License
 #
